dffd7e0df9049535161aedc5dbe90d55a45a40b9
ibrahim halil SOYDAN <ibrahimhalilsoydan@gmail.com>
2025-06-12 18:38:35 +0300

    added static files

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 import express from 'express';
+import ejs from "ejs"
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
+app.set("view engine","ejs");
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
+ res.render("index");
 });
+
+app.get('/about', (req, res) => {
+ res.render("about");
+});
+
+app.get('/add_post', (req, res) => {
+ res.render("add_post");
+});
+
+app.get('/post', (req, res) => {
+ res.render("post");
+});
+
+
+
 
 const port = 3000;
 app.listen(port, () => {
